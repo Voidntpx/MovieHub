4307fb8d24c58895768583629b9feeb34c631b12
Nattapat Sittichai <73817577+Voidntpx@users.noreply.github.com>
2025-03-04 18:01:44 +0700

    day 3

--- app/src/main/java/com/arise/training/moviehub/DetailActivity.kt
+++ app/src/main/java/com/arise/training/moviehub/DetailActivity.kt
@@ -1,66 +1,51 @@
 package com.arise.training.moviehub
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class SplashActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        lifecycleScope.launch {
-            delay(3000)
-            goToMainActivity()
-            finish()
-        }
     }
+
     override fun onStart() {
         super.onStart()
-        Log.d("testapp splash","onStart")
+        Log.d("testapp detail","onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("testapp splash","onResume")
+        Log.d("testapp detail","onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("testapp splash","onPause")
+        Log.d("testapp detail","onPause")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("testapp splash","onRestart")
+        Log.d("testapp detail","onRestart")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("testapp splash","onStop")
+        Log.d("testapp detail","onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-    }
-    fun goToMainActivity() {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra(EXTRA_NAME,"Frank Splash")
-        startActivity(intent)
-    }
-    companion object {
-        const val EXTRA_NAME = "EXTRA_NAME"
+        Log.d("testapp detail","onDestroy")
     }
 }
